5c2b2bec69a6f34fee8d0b88e5f140be561ff959
Jonny Wu <jonny.ww@foxmail.com>
2017-12-09 20:35:55 +0800

    the defination of basic type

---
+++ include/types.h
@@ -0,0 +1,26 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#ifndef NULL
+  #define NULL 0
+#endif
+
+#ifndef TRUE
+  #define TRUE 1
+  #define FALSE 0
+#endif
+
+/*
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef char int8_t;
+*/
+
+
+
+
+
+#endif //INCLUDE TYPES_H_
